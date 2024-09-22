990cce47ea8f4a5272cf81e281ff5e5f0731d2a0
malande2 <melnikov.alex.andreev@gmail.com>
2024-09-22 22:34:30 +0400

    fixed roll state

--- scenes/player/fsm/roll_state.gd
+++ scenes/player/fsm/roll_state.gd
@@ -4,14 +4,14 @@ class_name RollState
 @export var player: Player
 
 var direction: Vector2 = Vector2.ONE
-var target_position: Vector2
+var start_position: Vector2
 
-const SPEED = 600
+const SPEED = 400
 const DISTANCE = 40
 
 func enter():
+	start_position = player.position
 	direction = player.position.direction_to(player.get_global_mouse_position())
-	target_position = player.position + player.position.direction_to(player.get_global_mouse_position()) * DISTANCE
 	player.velocity = direction * SPEED
 	# disable a hurt box component
 
@@ -20,6 +20,6 @@ func exit():
 
 func physics_update(delta: float):
 	player.move_and_slide()
-	if player.position.distance_to(target_position) < 1.0 or player.get_slide_collision_count() > 0:
+	if player.position.distance_to(start_position) > DISTANCE or player.get_slide_collision_count() > 0:
 		Transitioned.emit(self, "controlled")
 		
